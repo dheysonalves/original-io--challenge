e194c84f9e247e6f63a0b4c462126281a949deb3
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-04-22 11:52:11 -0300

    feat: Adding the colors, fonts sizes and font family

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+const colors = {
+  'c-primary': '#DE8F75',
+  'c-secondary': '#5F75AA',
+  'c-complement1': '#AEB6C1',
+  'c-complement2': '#E0DA9C',
+  'c-complement3': '#A5C3C0',
+  'c-complement4': '#2A5A75',
+  'c-black': '#202020',
+  'c-white': '#FFFFFF',
+  'c-success': '#32917B',
+  'c-error': '#E35442',
+  'c-gray': '#F2F2F2',
+  'c-gray-light': '#E3E3E3',
+  'c-gray-lighter': '#CCCCCC',
+  'c-gray-dark': '#7F7F7F',
+  'c-gray-darker': '#686868',
+};
+
+export default colors;
---
+++ src/styles/fonts.ts
@@ -0,0 +1,11 @@
+const fonts = {
+	input: 16,
+	regular: 14,
+	extraLarger: 26,
+	larger: 18,
+	medium: 14,
+	small: 12,
+	tiny: 10,
+};
+
+export default fonts;
