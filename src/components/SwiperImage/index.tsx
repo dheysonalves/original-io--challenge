6b4e54e06f1d5aaa80d6782fead566145fbd18c9
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-08-16 19:43:46 -0300

    feat: Adiciona slide na tela de slide

--- src/components/SwiperImage/index.tsx
+++ src/components/SwiperImage/index.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable */
 
-import React, { Component } from 'react';
+import React, { Component, useState, useCallback, useReducer, useEffect } from 'react';
 
-import * as S from './styles';
+import * as S from './styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const arr = [
 	require('../../assets/img/1.jpg'),
@@ -11,36 +14,68 @@ const arr = [
 	require('../../assets/img/4.jpg')
 ]
 
-class index extends Component {
-
-
-	state = {
-		imgs: [
-			require('../../assets/img/1.jpg'),
-			require('../../assets/img/2.jpg'),
-			require('../../assets/img/3.jpg'),
-			require('../../assets/img/4.jpg')
-		],
-		currentIndex: 0,
-		movement: 0,
-	}
-
-	handleMovement() {
-
-	}
-
-
-	render() {
-		return (
-			<div>
-				<S.Container>
-					{this.state.imgs.map(src => {
-						return <S.Image src={src} key={src} width="100%" height="100%" />
-					})}
-				</S.Container>
-			</div>
-		);
+function reducer(state: any, action: any) {
+	switch(action.type) {
+		case 'one':
+			return {
+				image: require('../../assets/img/1.jpg'),
+				bool: false
+			};
+		case 'two':
+			return {
+				image: require('../../assets/img/2.jpg'),
+				bool: false
+			}
+		case 'three':
+			return {
+				image: require('../../assets/img/3.jpg'),
+				bool: false
+			};
+		case 'four':
+			return {
+				image: require('../../assets/img/4.jpg'),
+				bool: false
+		 }
+		case 'five':
+			return {
+				image: require('../../assets/img/5.jpg'),
+				bool: false
+		 };
+		default: {
+			return state;
+		}
 	}
 }
+
+// class Inde extends Components
+
+// componentDidMount() {}
+// componentDidUnMount() {}
+
+const index = () => {
+	const [value, setValue] = useState<boolean>(false);
+	const [state, dispatch] = useReducer(reducer, { image: require('../../assets/img/1.jpg'), bool: false });
+
+	const setPlaySlides = useCallback(() => {
+		setValue(!value);
+	}, [value, state.bool]);
+
+		return (
+			<S.Container>
+				<S.ImageBackground image={state?.image}>
+					<S.Swiper>
+						<S.PlayButton onClick={setPlaySlides}>
+							<FontAwesomeIcon icon={value ? faPauseCircle : faPlayCircle} size="lg" color="#5F75AA" />
+						</S.PlayButton>
+						<S.StepButton onClick={() => dispatch({ type: 'one' })}/>
+						<S.StepButton onClick={() => dispatch({ type: 'two' })}/>
+						<S.StepButton onClick={() => dispatch({ type: 'three' })}/>
+						<S.StepButton onClick={() => dispatch({ type: 'four' })}/>
+						<S.StepButton onClick={() => dispatch({ type: 'five' })}/>
+					</S.Swiper>
+				</S.ImageBackground>
+			</S.Container>
+		)
+	}
 
 export default index;
---
+++ src/components/SwiperImage/styles.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+const IMG_WIDTH = 250;
+const IMG_HEIGHT = 324;
+
+interface BackgroundProps {
+	image?: string;
+};
+
+export const Container = styled.div`
+	background-color: #0002;
+	overflow: hidden;
+	width: ${IMG_WIDTH}px;
+	height: ${IMG_HEIGHT}px;
+	margin: 0 auto;
+`;
+
+export const ImageBackground = styled.div<BackgroundProps>`
+	background-image: url(${props => props.image});
+	position: relative;
+	max-width: 100%;
+	height: 100%;;
+	background-position: center; /* Center the image */
+	background-repeat: no-repeat; /* Do not repeat the image */
+	background-size: cover; /* Resize the background image to cover the entire container */
+
+	::after {
+		content: "";
+		background-color: #0002;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+`;
+
+export const Swiper = styled.div`
+	position: absolute;
+	width: 100%;
+	height: auto;
+	bottom: 10px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	z-index: 2;
+`;
+
+export const PlayButton = styled.button`
+	height: auto;
+	border: none;
+	outline: none;
+	background-color: transparent;
+	margin: 0;
+	padding: 0;
+`;
+
+export const StepButton = styled.button`
+	background-color: transparent;
+	padding: 5px;
+	height: auto;
+	border-radius: 50%;
+	border: none;
+	outline: none;
+	border: 2px solid #fff;
+
+	:active {
+		padding: 6px;
+		background-color: #DE8F75;
+		border: none;
+	}
+	:focus {
+		padding: 6px;
+		background-color: #DE8F75;
+		border: none;
+	}
+`;
+
+export const Image = styled.img`
+	object-fit: fill;
+`;
