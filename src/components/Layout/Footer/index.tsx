79788af6097c2e4fe75327f430d7314307024a7c
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-09-01 00:59:21 -0300

    feat: Images wrapper

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,20 +1,114 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useCallback, useReducer,useState } from 'react';
 
 import Logotype from '../../Image';
 import Paragraph from '../../Paragraph';
 import * as S from './styles';
 
-function index() {
+const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case "state_one":
+            return { ...state, one: false };
+        case "state_two":
+            return { ...state, two: false };
+        case "state_three":
+            return { ...state, three: false };
+        default:
+            return state;
+    }
+};
+
+const index: React.FC = () => {
+    const [state, dispatch] = useReducer(reducer, {
+        one: false,
+        two: false,
+        three: false,
+    });
+
     return (
         <S.Container>
-            <Paragraph size={12} align="none" color="#fff">
-                Via Mia | V. Milano Centro Comercio de Bolsas Eireli - EPP. Av.das Américas, 500 - bloco 20, loja 126 - Barra da Tijuca - Rio de Janeiro - RJ - CEP: 22640-100 CNPJ: 05.292.288/0002-10 - I.E: 86.732.548 - E-mail: ecommerce@viamia.com.br
+            <S.ListWrapper>
+                <S.ListHeader>
+                    <Paragraph size={14} align="none" color="#AEB6C1" transform="uppercase">
+                        Institucional
+                    </Paragraph>
+                    <FontAwesomeIcon icon={faPlus} size="lg" color="#AEB6C1" style={{ marginRight: 10 }} onClick={() => dispatch({ type: "state_one", one: !state.one })} />
+                </S.ListHeader>
+                {
+                    state.one && (
+                        <S.UnorderedList>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                        </S.UnorderedList>
+                    )
+                }
+            </S.ListWrapper>
+            <S.ListWrapper>
+                <S.ListHeader>
+                    <Paragraph size={14} align="none" color="#AEB6C1" transform="uppercase">
+                        Informações
+                    </Paragraph>
+                    <FontAwesomeIcon icon={faPlus} size="lg" color="#AEB6C1" style={{ marginRight: 10 }} onClick={() => dispatch({ type: "state_two", two: !state.two })} />
+                </S.ListHeader>
+                {
+                    state.two && (
+                        <S.UnorderedList>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                        </S.UnorderedList>
+                    )
+                }
+            </S.ListWrapper>
+            <S.ListWrapper>
+                <S.ListHeader>
+                    <Paragraph size={14} align="none" color="#AEB6C1" transform="uppercase">
+                        Conheça
+                    </Paragraph>
+                    <FontAwesomeIcon icon={faPlus} size="lg" color="#AEB6C1" style={{ marginRight: 10 }} onClick={() =>
+                        dispatch({ type: "state_three", three: !state.three })} />
+                </S.ListHeader>
+                {
+                    state.three && (
+                        <S.UnorderedList>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                            <S.UnorderedItem>
+                                Teste 1
+                            </S.UnorderedItem>
+                        </S.UnorderedList>
+                    )
+                }
+            </S.ListWrapper>
+            <S.Infomation>
+                <Paragraph size={12} align="none" color="#fff">
+                    Via Mia | V. Milano Centro Comercio de Bolsas Eireli - EPP. Av.das Américas, 500 - bloco 20, loja 126 - Barra da Tijuca - Rio de Janeiro - RJ - CEP: 22640-100 CNPJ: 05.292.288/0002-10 - I.E: 86.732.548 - E-mail: ecommerce@viamia.com.br
             </Paragraph>
-            <S.SpacingDiv />
-            <Logotype src="https://source.unsplash.com/random" width="200px" height="60px" />
+                <S.SpacingDiv />
+                <Logotype src="https://source.unsplash.com/random" width="200px" height="60px" />
+            </S.Infomation>
         </S.Container>
     )
 }
 
-export default index
+export default index;
 
--- src/components/Layout/Footer/styles.ts
+++ src/components/Layout/Footer/styles.ts
@@ -1,24 +1,52 @@
 import styled from 'styled-components';
 
-type ButtonProps = {
-    color: string,
-    radius: string,
-    textColor: string,
-    eventBackground: string,
-    eventColor: string,
-    size: number,
-}
-
-
-export const Container = styled.div`
+export const Container = styled.footer`
     display:flex;
     flex-direction: column;
     align-items: center;
+`;
+
+export const Infomation = styled.div`
     background-color: #DE8F75;
     padding: 10px;
 `;
 
 export const SpacingDiv = styled.div`
     margin-top: 10px;
+    margin-bottom: 10px;
+`;
+
+export const ListWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+`;
+
+export const ListHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    padding-bottom: 10px;
+    padding-right: 20px;
+    padding-left: 20px;
+    border-bottom: 1px solid #ddd;
+`;
+
+export const UnorderedList = styled.ul`
+    text-decoration: none;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: 20px;
+    margin-right: 20px;
+`;
+
+
+export const UnorderedItem = styled.li`
+    font-size: 14px;
+    color: #202020;
     margin-bottom: 10px;
 `;
